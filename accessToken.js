df05dfaf7ecb778a2be8688c39c4f9bfe6a2176a
amgarton47 <amgb2018@mymail.pomona.edu>
2020-08-21 20:20:43 -0500

    deploy

---
+++ accessToken.js
@@ -0,0 +1,5 @@
+// mapbox gl access token
+// modularized so it can be gitignored and not sent to github
+process.token =
+  "pk.eyJ1IjoiYWdhcnRvbjQ3IiwiYSI6ImNrZTN1dzd2dTBtemsydXFuMDFxbHVrczYifQ.yexE44fgkZ8dFJ5fOIF7FA";
+export default "pk.eyJ1IjoiYWdhcnRvbjQ3IiwiYSI6ImNrZTN1dzd2dTBtemsydXFuMDFxbHVrczYifQ.yexE44fgkZ8dFJ5fOIF7FA";
